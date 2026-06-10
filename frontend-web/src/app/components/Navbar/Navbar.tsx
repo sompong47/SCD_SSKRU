9931c697c7e9e4edb78696233b8e380851941cbb
Suphan <148407664+SuphanChainok@users.noreply.github.com>
2026-06-10 09:38:44 +0700

    logo_one

--- frontend-web/src/app/components/Navbar/Navbar.tsx
+++ frontend-web/src/app/components/Navbar/Navbar.tsx
@@ -15,7 +15,6 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-24">
           
-          {/* โลโก้และชื่อเว็บ */}
           <div className="flex items-center gap-4">
             <Image
               src="/logo_sskru.jpg"
